82f4972088ceb0456eac2dcb144ecf696f734ad8
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-15 14:53:23 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+import React from "react"
+function Welcome(props){
+return (
+  <>
+    <h2>{"Welcome to Newton School."}</h2>
+    <h1>{`Hey ! ${props.name}`}</h1>
+  <>
+)
+}
